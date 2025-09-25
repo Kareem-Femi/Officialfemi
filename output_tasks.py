d6b70eae084e74735654d66f9b75e3f6a0b09847
FEMI KAREEM <0karef39@solent.ac.uk>
2025-09-25 14:50:47 +0100

    testing backdashes

---
+++ output_tasks.py
@@ -0,0 +1,15 @@
+from tokenize import blank_re
+
+print("welcome to com411!")
+#to make your own notes and it won't display
+print("In week 1 we will learn...")
+print("\n...How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
+print("\nI hope you are enjoying the lesson thus far!")
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
+print("\"I am programming\"")
